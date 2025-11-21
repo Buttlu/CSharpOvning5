999d089c901f2a354a5c8537f185e199492124de
Jacob <jackan202@gmail.com>
2025-11-21 13:44:26 +0100

    Added a few empty methods in GarageHandler to handle basic functionality

---
+++ CSharpOvning5/GarageHandler.cs
@@ -0,0 +1,40 @@
+﻿using CSharpOvning5.Vehicles;
+using System.Drawing;
+
+namespace CSharpOvning5;
+
+public class GarageHandler(int capacity, IUI ui) : IHandler
+{
+    private IUI _ui = ui;
+    private Garage<Vehicle> _garage = new(capacity);
+
+    public void DisplayGarageVehicles()
+    {
+
+    }
+
+    public void CountVehicleTypes()
+    {
+
+    }
+
+    public bool AddVehicle(Vehicle vehicle)
+    {
+        return true;
+    }
+
+    public bool RemoveVehicle(Vehicle vehicle)
+    {
+        return true;
+    }
+
+    public bool SeedGarage()
+    {
+        return true;
+    }
+
+    public IEnumerable<Vehicle> SearchForVehicles()
+    {
+        return null;
+    }
+}
