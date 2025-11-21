e105aa97c6787c893c4f8bc98af05512b61ff231
Jacob <jackan202@gmail.com>
2025-11-21 15:24:17 +0100

    Added methods for adding and viewing vehicles in the garage

--- CSharpOvning5/Garage.cs
+++ CSharpOvning5/Garage.cs
@@ -3,14 +3,24 @@ using System.Collections;
 
 namespace CSharpOvning5;
 
-internal class Garage<T>(int capacity) : IEnumerable<T> where T : Vehicle
+public class Garage<T>(int capacity) : IEnumerable<T> where T : Vehicle
 {
     private readonly T[] _garage = new T[capacity];
+    private int _index = 0;
+
+    public bool Add(T vehicle)
+    {
+        if (_index >= capacity)
+            return false;
+
+        _garage[_index++] = vehicle;
+        return true;
+    }
 
     public IEnumerator<T> GetEnumerator()
     {
-        foreach (T vehicle in _garage) {
-            yield return vehicle;
+        for (int i = 0; i < _index; i++) {
+            yield return _garage[i];
         }
     }
 
