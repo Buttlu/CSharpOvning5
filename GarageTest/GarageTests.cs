a5467af189b4eac84bc44bf522cd549bc217c99c
Jacob <jackan202@gmail.com>
2025-11-28 15:33:20 +0100

    Added unit test to check for garage overflow

---
+++ GarageTest/GarageTests.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using CSharpOvning5.GarageClasses;
+using CSharpOvning5.Vehicles;
+using System.Drawing;
+
+namespace UnitTest;
+
+public class GarageTests
+{
+    private readonly Mock<Vehicle> _mockVehicle = new("ABC123", Color.White, (uint)0);
+
+
+
+    [Fact]
+    public void Add_AddsVehiclesPastLimit()
+    {
+        int limit = 5;
+        IGarage<Vehicle> garage = new Garage<Vehicle>(limit);
+        for (int i = 0; i < limit; i++) {
+            garage.Add(_mockVehicle.Object);
+        }
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => garage.Add(_mockVehicle.Object));
+    }
+}
