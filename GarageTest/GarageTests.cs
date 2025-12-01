94db7df3fefd4cd2665d5635e25796904405ea38
Jacob <jackan202@gmail.com>
2025-12-01 12:04:18 +0100

    Simplified certain tests

--- GarageTest/GarageTests.cs
+++ GarageTest/GarageTests.cs
@@ -9,34 +9,20 @@ public class GarageTests
 {
     private const string _validLicenseNumber = "ABC123";
     private const string _invalidLicenseNumber = "ABC12";
-    private const int _limit = 5;
+    private const int _limit = 5;    
     private readonly Mock<Vehicle> _mockVehicle = new(_validLicenseNumber, Color.White, (uint)0);
-
-    // Since the length is always the same, 
-    // the amount of vehicles have to be manually counted
-    private static int CountVehicles(IGarage<Vehicle> garage)
-    {
-        int count = 0;
-        foreach (var vehicle in garage) {
-            if (vehicle is not null)
-                count++;
-        }
-        return count;
-    }
-
+    
     [Fact]
     public void Add_AddVehicle_LengthIncreased()
     {
-        // Checks if something happened by counting the number of vehicles in the garage
+        // Need to specifially test the Add method, simplified initialization bypasses Add.
+#pragma warning disable IDE0028 // Simplify collection initialization
         IGarage<Vehicle> garage = new Garage<Vehicle>(_limit);
-        int prevCount = CountVehicles(garage);
+#pragma warning restore IDE0028 // Simplify collection initialization
 
         garage.Add(_mockVehicle.Object);
 
-        int afterCount = CountVehicles(garage);
-
-        // Check that the vehicle count was increased by 1
-        Assert.Equal(prevCount+1, afterCount);
+        Assert.Contains(_mockVehicle.Object, garage);
     }
 
     [Fact]
@@ -63,20 +49,14 @@ public class GarageTests
     [Fact]
     public void Remove_RemoveFoundVehicle_LengthDecreases()
     {
-        IGarage<Vehicle> garage = new Garage<Vehicle>(_limit);
         // Needs to add a vehicle in order to be able to remove it
         // If Adding fails, the test can't contine so just immediately exit
-        int prevCount = CountVehicles(garage);
-        garage.Add(_mockVehicle.Object);
-        int afterCount = CountVehicles(garage);
-        if ((prevCount + 1) != afterCount)
-            Assert.Fail("Could not add vehicle to the garage. Cannot continue");
+        IGarage<Vehicle> garage = new Garage<Vehicle>(_limit) {
+            _mockVehicle.Object
+        };
 
-        prevCount = afterCount;
         garage.Remove(_mockVehicle.Object.LicenseNumber);
-        afterCount = CountVehicles(garage);
-        // Check that the vehicle count was decreased by 1
-        Assert.Equal(prevCount-1, afterCount);
+        Assert.DoesNotContain(_mockVehicle.Object, garage);
     }
 
     [Theory]
